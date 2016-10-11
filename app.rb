fd3138be449f12b1898b17d42038ff014786410f
Daria Sova <todariasova@gmail.com>
2016-10-11 00:02:45 -0700

    Now can tweet from a user's account end
    Change-Id: Ia9c8e3bafa0c480a1a720b7b1f4912320e0e5e25

--- app.rb
+++ app.rb
@@ -14,24 +14,22 @@ get '/login' do
   session[:token] = @request_token.token
   session[:token_secret] = @request_token.secret
   redirect @request_token.authorize_url(:oauth_callback => @callback_url)
-
-
-  #client = Twitter::REST::Client.new do |config|
-  #  config.consumer_key        = "4QYCvDdOhBZTiaqLH3kv4LvAW"
-  #  config.consumer_secret     = "nBX51piLcNMn64QGoRdnbP6RS8sSsOvQYt9wSyTzWfYX36nx9G"
-  #  config.access_token        = request_token.token
-  #  config.access_token_secret = request_token.secret
-  #end
-  #binding.pry
-  #client.update('Tweeting with OAuth access!')
 end
 
 get '/oauth/twitter/callback' do 
-  "CAllback REcieved!!"
   hash = { oauth_token: params[:oauth_token], oauth_token_secret: params[:oauth_verifier]}
   @consumer = OAuth::Consumer.new("4QYCvDdOhBZTiaqLH3kv4LvAW","nBX51piLcNMn64QGoRdnbP6RS8sSsOvQYt9wSyTzWfYX36nx9G", :site => "https://api.twitter.com")
   request_token  = OAuth::RequestToken.from_hash(@consumer, hash)
   @access_token = request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
+  @client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "4QYCvDdOhBZTiaqLH3kv4LvAW"
+    config.consumer_secret     = "nBX51piLcNMn64QGoRdnbP6RS8sSsOvQYt9wSyTzWfYX36nx9G"
+    config.access_token        = @access_token.token
+    config.access_token_secret = @access_token.secret
+  end
+  #tweet from user's account
+  @client.update('Hola!')
+  "Woohoo! it works!"
 end
 
 #private
