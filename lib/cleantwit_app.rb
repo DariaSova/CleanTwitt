65fac4f29c8a4a6144d8e7afb0530c3a97420068
Daria Sova <todariasova@gmail.com>
2017-04-02 21:21:16 -0700

    Fetch and delete tweets
    Change-Id: I4bda7ff950299928c9d042f41a24177e324fb2cc

--- lib/cleantwit_app.rb
+++ lib/cleantwit_app.rb
@@ -5,6 +5,27 @@ class CleanTwitApp
   end
 
   def delete_all_tweets
-    #@client.update('yay!')
+    tweets_total = @client.user.statuses_count
+    user_id = @client.user.id
+    count = tweets_total
+    puts "Total tweeets count: #{count}"
+
+    begin
+      while count > 0 do
+        tweets = @client.user_timeline(user_id, {:count => 200})
+        batch = tweets.length
+        tweets.each do  |t|
+          if @client.destroy_status(t.id)
+            puts "Status #{t.id} deleted."
+          end
+        end
+        puts "DELETED #{batch} TWEETS"
+        count-=batch
+        #if we can't get tweets
+        break if batch==0
+      end
+    end
+  rescue Exception => e
+    puts "Opps..something went wrong #{e}!"
   end
 end
